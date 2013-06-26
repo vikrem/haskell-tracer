d1252d1a5cf121b19738f467bde9d8c73d0eda5c
Vikrem <hello@vikrem.com>
2013-06-25 23:43:06 -0700

    Fix up some shader stuff

--- main.hs
+++ main.hs
@@ -33,14 +33,22 @@ cameraShader :: Colour -> ShaderFunc
 cameraShader diffuseColor hitpoint surf = map ( round . ( (*) ratio) . fromIntegral  ) $ diffuseColor
 				where
 				ratio = abs $ dot (normalize diffvec) (normalize normal)
-				diffvec = cameraOrigin - (origin surf)
+				diffvec = cameraOrigin - hitpoint
 				normal = hitpoint - origin surf
+
+cameraLinFalloffShader :: Colour -> Scalar -> ShaderFunc
+cameraLinFalloffShader diffuseColor luminosity hitpoint surf = map ( round . ( (*) ratio) . fromIntegral  ) $ diffuseColor
+				where
+				ratio = min (luminosity / (sqr $ mag diffvec)) 1.0
+				diffvec = cameraOrigin - hitpoint
+				sqr x = x * x
 
 emissionShader :: Colour -> ShaderFunc
 emissionShader emitcolour _ _ = emitcolour
 
 scene = [ 
-		  (Surface (Vector [0.7, 0.7, 2]) (sphereHit 0.25) (emissionShader [255, 255, 255]))
+		  (Surface (Vector [0.7, 0.7, 1]) (sphereHit 0.25) (cameraShader [0, 165, 255] )),
+		  (Surface (Vector [0.3, 0.3, 2]) (sphereHit 0.25) (cameraShader [255, 165, 0] ))
 		  ]
 width = 800
 height = 800
