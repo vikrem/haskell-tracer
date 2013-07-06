428f34b987096c88faa3ca8c2f8f352c67e37a13
Vikrem <hello@vikrem.com>
2013-07-06 16:11:19 -0700

    Remove shader class

--- lib/shader.hs
+++
@@ -1,8 +0,0 @@
-module Lib.Shader where
-import Lib.Vector
-import Lib.Image
--- Define shadeable
-
-class Shader a where
-	-- Take in a hitpoint, and a normal. Return a color
-	shaderFunc :: a -> Vector -> Vector -> Colour
